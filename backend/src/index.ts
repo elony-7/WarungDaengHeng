2d2316b10d8b6d231f3acdd9fe29365f71e84217
elony-7 <elony7@outlook.com>
2024-07-17 22:54:51 +0800

    Add files via upload
    backend and frontend

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log("Connected to Database!"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: express.Request, res: express.Response) => {
+    res.json({ message: "Hello!" });
+});
+
+app.listen(7000, () => {
+    console.log("server started on localhost:7000");
+});
