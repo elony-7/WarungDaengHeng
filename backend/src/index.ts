f0e45287a01d2e95406c092a13f81a684ce3f30a
elony-7 <elony7@outlook.com>
2024-07-19 17:20:43 +0800

    Add files via upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,28 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => console.log("Connected to Database!"));
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Database!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: express.Request, res: express.Response) => {
-    res.json({ message: "Hello!" });
+// Define CSP policy
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self' http://localhost:7000"
+  );
+  next();
 });
+// /api/my/user
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
-    console.log("server started on localhost:7000");
+  console.log("server started on localhost:7000");
 });
